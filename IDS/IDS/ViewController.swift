307eb01ab2460ab42c0cf5fb62fc4fee419fb429
Shoeb <138053107+shoeblifecycle@users.noreply.github.com>
2025-04-22 11:26:28 +0530

    Hi commit

---
+++ IDS/IDS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IDS
+//
+//  Created by IDS Logic on 22/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IDS/IDSTests/IDSTests.swift
@@ -0,0 +1,17 @@
+//
+//  IDSTests.swift
+//  IDSTests
+//
+//  Created by IDS Logic on 22/04/25.
+//
+
+import Testing
+@testable import IDS
+
+struct IDSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
